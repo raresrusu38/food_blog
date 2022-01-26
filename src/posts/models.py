18949f2a2552dd6e81798978f2f30dfcc645c57b
raresrusu38 <rares.rusu29@gmail.com>
2022-01-26 15:38:56 +0200

    added Post model and query data with classed based views

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Post(models.Model):
+    title           = models.CharField(max_length=150)
+    content         = models.TextField()
+    publishing_date = models.DateTimeField(auto_now_add=True)
+    image           = models.ImageField(upload_to='posts/', blank=True, null=True)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+
+    
+    class Meta:
+        verbose_name        = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering            = ('id',)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'posts/index.html'
+    model = Post
+    context_object_name = 'posts'
+    
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
