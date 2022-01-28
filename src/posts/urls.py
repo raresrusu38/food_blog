96ad94bcd79f149b36ee6c7db14a564c1de3e680
raresrusu38 <rares.rusu29@gmail.com>
2022-01-28 18:21:45 +0200

    added post-detail page

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PostDetail
 
 app_name = "posts"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('detail/<int:pk>/', PostDetail.as_view(), name='post-detail'),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -13,3 +13,15 @@ class IndexView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         return context
+    
+    
+class PostDetail(DetailView):
+    template_name = 'posts/post-detail.html'
+    model = Post
+    context_object_name = 'single'
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        return context
+    
