d36fe1a792f0e5fb4544e99c9489530c92829154
raresrusu38 <rares.rusu29@gmail.com>
2022-01-29 19:14:15 +0200

    added category model, category-detail and templates

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls                    import path
-from .views                         import IndexView, PostDetail
+from .views                         import CategoryDetail, IndexView, PostDetail
 
 app_name = "posts"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('detail/<int:pk>/<slug:slug>/', PostDetail.as_view(), name='post-detail'),
+    path('category/<int:pk>/<slug:slug>/', CategoryDetail.as_view(), name='category-detail'),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -25,3 +25,18 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data(**kwargs)
         return context
     
+
+class CategoryDetail(ListView):
+    model = Post
+    template_name = 'categories/category-detail.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'], slug=self.kwargs['slug'])
+        return Post.objects.filter(category=self.category).order_by('-id')
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetail, self).get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'], slug=self.kwargs['slug'])
+        context['category'] = self.category
+        return context
